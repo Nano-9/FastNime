ab039eb174ee2556ec5bece29a34582901b3f246
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2021-08-18 13:25:23 -0300

    Update entrada.py

--- FastNime_Termux/entrada.py
+++ FastNime_Termux/entrada.py
@@ -1,5 +1,8 @@
+import os
+import sys
 import rich
 from rich.console import Console
+from time import sleep
 console = Console()
 def inicio():
 	console.print("""
@@ -18,6 +21,24 @@ def inicio():
 [ 2 ] - Fazer download dos episódios
 [ 3 ] - Sair
 		""")
-
-	escolha1 = int(input("O que deseja: "))
-	return escolha1
+	try:
+		escolha1 = int(input("O que deseja: "))
+	except ValueError:
+		print("\nO valor inserido não é suportado! Por favor, informe um número inteiro!")
+	except KeyboardInterrupt:
+		print("\nO usuário resolveu fechar o programa!")
+		sleep(1)
+		sys.exit()
+	else:
+		while escolha1 <= 0 or escolha1 > 3:
+			print("Opção inválida!")
+			sleep(1)
+			try:
+				escolha1 = int(input("O que deseja: "))
+			except ValueError:
+				print("\nO valor inserido não é suportado! Por favor, informe um número inteiro!")
+			except KeyboardInterrupt:
+				print("\nO usuário resolveu fechar o programa!")
+				sleep(1)
+				sys.exit()
+		return escolha1
